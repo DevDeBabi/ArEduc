f9a3f9b9ef6193690b05f5461a3681effbc61d59
KaboreIssouf <kaboreissouf.wk@gmail.com>
2023-03-22 15:29:04 +0000

    jeux

---
+++ Assets/Scenes/FRUIT SCENE ENG/JeuxFruit_Manager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeuxFruit_Manager : MonoBehaviour
+{
+    public bool commencer;
+    public bool orange, banane, ananas, mangue, pomme;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
