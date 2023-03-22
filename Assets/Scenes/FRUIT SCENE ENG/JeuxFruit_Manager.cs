fbe315affc32ef490b29aca12ff1dd48cf688a99
KaboreIssouf <kaboreissouf.wk@gmail.com>
2023-03-22 17:04:37 +0000

    jeux

--- Assets/Scenes/FRUIT SCENE ENG/JeuxFruit_Manager.cs
+++ Assets/Scenes/FRUIT SCENE ENG/JeuxFruit_Manager.cs
@@ -1,19 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JeuxFruit_Manager : MonoBehaviour
 {
+    public List<Sprite> lesImage;
+    public Sprite le_Fruit_Choisi;
+
+    public List<Image> lesPlaces;
+
+    public bool orange, papaye, ananas, mangue, pomme;
+     int nombre_Total = 40;
+    public int nombre_pour_le_fruit_concerner;
+     int restant;
+
     public bool commencer;
-    public bool orange, banane, ananas, mangue, pomme;
-    void Start()
+    public List<Image> xxx;
+
+    private void Awake()
     {
-        
+        for (int i = 0; i < lesPlaces.Count; i++)
+        { 
+            lesPlaces[i].gameObject.SetActive(false);
+        }
+        nombre_pour_le_fruit_concerner = Random.Range(5, 25);
+        restant = nombre_Total - nombre_pour_le_fruit_concerner;
+
+        int d = Random.Range(0, 5);
+
+        if(d==0)
+        {
+            orange = true;
+        }
+        if (d == 1)
+        {
+            papaye = true;
+        }
+        if (d == 2)
+        {
+            ananas = true;
+        }
+        if (d == 3)
+        {
+            mangue = true;
+        }
+        if (d >= 4)
+        {
+            pomme = true;
+        }
+
+        le_Fruit_Choisi = lesImage[d];
+
+        lesImage.RemoveAt(d);
+
+
+        InvokeRepeating("ajout", 0.1f, 0.2f);
     }
 
-    // Update is called once per frame
-    void Update()
+    int y;
+    void ajout()
     {
-        
+        int id = Random.Range(0, lesPlaces.Count);
+        xxx.Add(lesPlaces[id]);
+        lesPlaces.RemoveAt(id);
+
+        y += 1;
+        if(y== restant)
+        {
+            CancelInvoke();
+            for (int i = 0; i < lesPlaces.Count; i++)
+            { //les bons
+                lesPlaces[i].sprite = le_Fruit_Choisi;
+                lesPlaces[i].GetComponent<onImage>().isCorrect = true;
+                lesPlaces[i].gameObject.SetActive(true);
+            }
+
+            for (int i = 0; i < xxx.Count; i++)
+            {
+                xxx[i].sprite = lesImage[Random.Range(0, lesImage.Count)];
+                xxx[i].gameObject.SetActive(true);
+            }
+        }
+    }
+
+    
+
+   
+    public void Verification(bool verif)
+    {
+        if(verif)
+        {
+            print("bonne reponse");
+        }
+        else
+        {
+            print("mauvaise reponse");
+        }
     }
 }
---
+++ Assets/Scenes/FRUIT SCENE ENG/onImage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class onImage : MonoBehaviour
+{
+    public bool isCorrect;
+    public Button moi;
+
+    void Start()
+    {
+        moi = GetComponent<Button>();
+        moi.onClick.AddListener(goVerif);
+    }
+
+
+    void goVerif()
+    {
+
+        GameObject.FindObjectOfType<JeuxFruit_Manager>().Verification(isCorrect);
+        if (isCorrect)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
